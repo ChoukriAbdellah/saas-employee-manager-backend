512487108c95bdef3eb66f4c24bbccfac2732e75
Choukri abdellah <33703036+ChoukriAbdellah@users.noreply.github.com>
2021-10-03 17:21:52 +0200

    Update SassEmployeeManagerApplication.java

--- src/main/java/tech/getarrays/sass_employee_manager/SassEmployeeManagerApplication.java
+++ src/main/java/tech/getarrays/sass_employee_manager/SassEmployeeManagerApplication.java
@@ -20,7 +20,7 @@ public class SassEmployeeManagerApplication {
 	public CorsFilter corsFilter() {
 		CorsConfiguration corsConfiguration = new CorsConfiguration();
 		corsConfiguration.setAllowCredentials(true);
-		corsConfiguration.setAllowedOrigins(Arrays.asList("*"));
+		corsConfiguration.setAllowedOrigins(Arrays.asList("https://em-frontend-angular.herokuapp.com/"));
 		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
 				"Accept", "Authorization", "Origin, Accept", "X-Requested-With",
 				"Access-Control-Request-Method", "Access-Control-Request-Headers"));
