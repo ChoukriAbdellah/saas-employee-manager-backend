1912d3469fab4dd0633d402b95a22e990a0e271f
choukri <abdellahchoukri2.0@gmail.com>
2021-10-11 17:59:35 +0200

    add multiple origins in cros config

--- src/main/java/tech/getarrays/sass_employee_manager/SassEmployeeManagerApplication.java
+++ src/main/java/tech/getarrays/sass_employee_manager/SassEmployeeManagerApplication.java
@@ -7,7 +7,9 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 @SpringBootApplication
 public class SassEmployeeManagerApplication {
@@ -18,9 +20,12 @@ public class SassEmployeeManagerApplication {
 
 	@Bean
 	public CorsFilter corsFilter() {
+		List<String> origins = new ArrayList<String>();
+		origins.add("http://localhost:4200");
+		origins.add("https://em-frontend-angular.herokuapp.com/");
 		CorsConfiguration corsConfiguration = new CorsConfiguration();
 		corsConfiguration.setAllowCredentials(true);
-		corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+		corsConfiguration.setAllowedOrigins(origins);
 		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
 				"Accept", "Authorization", "Origin, Accept", "X-Requested-With",
 				"Access-Control-Request-Method", "Access-Control-Request-Headers"));
